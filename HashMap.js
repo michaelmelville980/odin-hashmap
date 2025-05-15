71441add5b3a359d91b0b20eb54b8dc73da0f0f6
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-05-15 13:27:18 -0400

    adds HashMap class (with constructor and hash function) and links with main.js/index.html

---
+++ HashMap.js
@@ -0,0 +1,25 @@
+export default class HashMap{
+
+    constructor(capacity, loadFactor){
+        this.buckets = Array.from({length: capacity}, () => []);
+        this.loadFactor = loadFactor;
+        this.capacity = capacity;
+    }
+
+    hash(key) {
+    let hashCode = 0;
+        
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+        hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+    } 
+
+
+
+
+
+
+}
